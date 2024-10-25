5468b0b6b6e6275fe39d75a882e241dd3246bf47
Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
2024-10-25 10:32:06 +0200

    Re-added User
    Refactored the User class and it's Validator

---
+++ src/core/domain/models/user/User.cs
@@ -0,0 +1,155 @@
+﻿using System.ComponentModel.DataAnnotations;
+using OperationResult;
+
+namespace domain.models.user;
+
+/// <summary>
+/// This class represents a user within the system.
+/// </summary>
+public class User
+{
+    // # METADATA #
+
+    /// <summary>
+    /// The ID of the user.
+    /// </summary>
+    [Key]
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// When the user was created.
+    /// </summary>
+    public DateTime CreatedAt { get; private set; }
+
+    /// <summary>
+    /// The last time the user was updated. (Default is DateTime.MinValue, if the user has never been updated.)
+    /// </summary>
+    public DateTime UpdatedAt { get; private set; } = DateTime.MinValue;
+
+    // # PROPERTIES #
+
+    [Required]
+    [MaxLength(50)]
+    [MinLength(2)]
+    public string FirstName { get; private set; }
+
+    [Required]
+    [MaxLength(60)]
+    [MinLength(2)]
+    public string LastName { get; private set; }
+
+    [Required]
+    [MaxLength(60)]
+    public string Email { get; private set; }
+
+    // # CONSTRUCTORS #
+    private User(string firstName, string lastName, string email)
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    public static Result<User> Create(string firstName, string lastName, string email)
+    {
+        // ! Validate the user's input here.
+        var validationResult = Validate(firstName, lastName, email);
+
+        // ? Is the validation a failure?
+        if (validationResult.IsFailure)
+            return Result<User>.Failure(validationResult.Errors.ToArray());
+
+        return new User(firstName, lastName, email);
+    }
+
+    private static Result Validate(string firstName, string lastName, string email)
+    {
+        // ! Validate the first name.
+        var firstNameResult = UserPropertyValidator.ValidateFirstName(firstName);
+
+        // ? Is the result a failure?
+        if (firstNameResult.IsFailure)
+        {
+            return Result.Failure(firstNameResult.Errors.ToArray());
+        }
+
+        // ! Validate the last name.
+        var lastNameResult = UserPropertyValidator.ValidateLastName(lastName);
+
+        // ? Is the result a failure?
+        if (lastNameResult.IsFailure)
+        {
+            return Result.Failure(lastNameResult.Errors.ToArray());
+        }
+
+        // ! Validate the email.
+        var emailResult = UserPropertyValidator.ValidateEmail(email);
+
+        // ? Is the result a failure?
+        if (emailResult.IsFailure)
+        {
+            return Result.Failure(emailResult.Errors.ToArray());
+        }
+
+        return Result.Success();
+    }
+
+    // # METHODS #
+
+    public Result UpdateFirstName(string firstName)
+    {
+        // ! Validate the first name.
+        var result = UserPropertyValidator.ValidateFirstName(firstName);
+
+        // ? Is the result a failure?
+        if (result.IsFailure)
+        {
+            return Result.Failure(result.Errors.ToArray());
+        }
+
+        // * Update the first name.
+        FirstName = firstName;
+        UpdatedAt = DateTime.UtcNow;
+
+        return Result.Success();
+    }
+
+    public Result UpdateLastName(string lastName)
+    {
+        // ! Validate the last name.
+        var result = UserPropertyValidator.ValidateLastName(lastName);
+
+        // ? Is the result a failure?
+        if (result.IsFailure)
+        {
+            return Result.Failure(result.Errors.ToArray());
+        }
+
+        // * Update the last name.
+        LastName = lastName;
+        UpdatedAt = DateTime.UtcNow;
+
+        return Result.Success();
+    }
+
+    public Result UpdateEmail(string email)
+    {
+        // ! Validate the email.
+        var result = UserPropertyValidator.ValidateEmail(email);
+
+        // ? Is the result a failure?
+        if (result.IsFailure)
+        {
+            return Result.Failure(result.Errors.ToArray());
+        }
+
+        // * Update the email.
+        Email = email;
+        UpdatedAt = DateTime.UtcNow;
+
+        return Result.Success();
+    }
+}
---
+++ src/core/domain/models/user/UserPropertyValidator.cs
@@ -0,0 +1,138 @@
+﻿using System.Text.RegularExpressions;
+using domain.exceptions;
+using OperationResult;
+
+namespace domain.models.user;
+
+/// <summary>
+/// This class handles validation all properties relating to the <see cref="User"/> class.
+/// </summary>
+public static class UserPropertyValidator
+{
+    /// <summary>
+    /// Validates the first name of a user.
+    /// </summary>
+    /// <param name="firstName">First name to be validated.</param>
+    /// <returns>A <see cref="Result"/> indicating if first name didn't pass the validation rules.</returns>
+    public static Result<string> ValidateFirstName(string firstName)
+    {
+        // ? Is the first name empty?
+        if (string.IsNullOrWhiteSpace(firstName))
+            return Result<string>.Failure(new InvalidArgumentException("First name cannot be empty, please provide a first name."));
+
+        // ? Does the first name contain any non-alphabetic characters?
+        if (!Regex.IsMatch(firstName, @"^[a-zA-Z\s]+$"))
+            return Result<string>.Failure(new InvalidArgumentException("First name can only contain alphabetic characters and spaces."));
+
+        // ? Does the first name contain extra spaces?
+        if (firstName.Trim() != firstName || firstName.Contains("  "))
+            return Result<string>.Failure(new InvalidArgumentException("First name cannot contain leading, trailing, or multiple consecutive spaces."));
+
+        // ? Is the first name too short?
+        if (firstName.Length < 2)
+            return Result<string>.Failure(new InvalidArgumentException("First name is too short, please provide a first name with at least 2 characters."));
+
+        // ? Is the first name too long?
+        if (firstName.Length > 50)
+            return Result<string>.Failure(new InvalidArgumentException("First name is too long, please provide a first name with at most 50 characters."));
+
+        return Result<string>.Success(firstName);
+    }
+
+    /// <summary>
+    /// Validates the last name of a user.
+    /// </summary>
+    /// <param name="lastName">Last name to be validated.</param>
+    /// <returns>A <see cref="Result"/> indicating if last name didn't pass the validation rules. </returns>
+    public static Result<string> ValidateLastName(string lastName)
+    {
+        // ? Is the last name empty?
+        if (string.IsNullOrWhiteSpace(lastName))
+            return Result<string>.Failure(new InvalidArgumentException("Last name cannot be empty, please provide a last name."));
+
+        // ? Does the last name contain any non-alphabetic characters?
+        if (!Regex.IsMatch(lastName, @"^[a-zA-Z\s]+$"))
+            return Result<string>.Failure(new InvalidArgumentException("Last name can only contain alphabetic characters and spaces."));
+
+        // ? Does the last name contain extra spaces?
+        if (lastName.Trim() != lastName || lastName.Contains("  "))
+            return Result<string>.Failure(new InvalidArgumentException("Last name cannot contain leading, trailing, or multiple consecutive spaces."));
+
+        // ? Is the last name too short?
+        if (lastName.Length < 2)
+            return Result<string>.Failure(new InvalidArgumentException("Last name is too short, please provide a last name with at least 2 characters."));
+
+        // ? Is the last name too long?
+        if (lastName.Length > 60)
+            return Result<string>.Failure(new InvalidArgumentException("Last name is too long, please provide a last name with at most 60 characters."));
+
+        return Result<string>.Success(lastName);
+    }
+
+    /// <summary>
+    /// Validates the email of a user.
+    /// </summary>
+    /// <param name="email">Email to be checked</param>
+    /// <returns>A <see cref="Result"/> indicating if email didn't pass the validation rules.</returns>
+    public static Result<string> ValidateEmail(string email)
+    {
+        // ? Is the email empty?
+        if(string.IsNullOrWhiteSpace(email))
+            return Result<string>.Failure(new InvalidArgumentException("Email cannot be empty, please provide an email."));
+
+        if(!EmailFormatCheck(email))
+            return Result<string>.Failure(new InvalidArgumentException("Email is invalid, please provide a valid email."));
+
+        return Result<string>.Success(email);
+    }
+
+    /// <summary>
+    /// Checks if the email is in the correct format.
+    /// </summary>
+    /// <param name="value">Email to be checked.</param>
+    /// <returns>a <see cref="bool"/> indicating if the email is in the correct format.</returns>
+    private static bool EmailFormatCheck(string value)
+    {
+        const string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+        // ? Is the email in the correct format?
+        if (!Regex.IsMatch(value, pattern))
+        {
+            return false;
+        }
+
+        // ? Is the email too long?
+        var parts = value.Split('@');
+        if (parts.Length != 2)
+        {
+            return false;
+        }
+
+
+        var localPart = parts[0];
+        var domainPart = parts[1];
+
+        // ? Is the local part or domain part too long?
+        if (localPart.Length > 30 || value.Length > 254)
+        {
+            return false;
+        }
+
+        // ? Does the local part or domain part contain ".."?
+        if (localPart.Contains("..") || domainPart.Contains(".."))
+        {
+            return false;
+        }
+
+        // ? Does the local part start or end with a period?
+        if (localPart.StartsWith('.') || localPart.EndsWith('.'))
+        {
+            return false;
+        }
+
+        // ? Is the domain part too long?
+        var domainParts = domainPart.Split('.');
+
+        return !domainParts.Any(part => part.Length > 63);
+    }
+}
