b08b02aff68a602feb28a6f741cc25651e25f2b4
Mads Hylleberg Nielsen <102622934+MalasataXD@users.noreply.github.com>
2024-11-11 10:38:49 +0100

    Workspace Endpoints now use the UserDTO
    The workspace endpoints now also use the new and improved UserDTO

--- src/presentation/api/endpoints/workspace/GetAllWorkspacesEndpoint.cs
+++ src/presentation/api/endpoints/workspace/GetAllWorkspacesEndpoint.cs
@@ -40,7 +40,7 @@ public class GetAllWorkspacesEndpoint(ICommandDispatcher dispatcher) : EndpointB
     private DTOs.WorkspaceDTO TransformSingle(Workspace workspace)
     {
         // * Extract the contacts from the workspace
-        var contacts = workspace.Contacts.Select(contact => new DTOs.UserDTO(contact.Id.ToString(), contact.FirstName, contact.LastName, contact.Email)).ToList();
+        var contacts = workspace.Contacts.Select(contact => new UserDTO(contact.Id.ToString(), contact.FirstName, contact.LastName, contact.Email,[],[])).ToList();
 
         // * Create the DTO
         return new DTOs.WorkspaceDTO(workspace.Id.ToString(), workspace.Title, workspace.Owner.Name, contacts, workspace.Projects.Select(project => project.Id.ToString()).ToList());
